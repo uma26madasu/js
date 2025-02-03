c8860d216e1ef85480cd0cb7485a6f618072b65c
uma26madasu <57607368+uma26madasu@users.noreply.github.com>
2025-02-03 11:46:19 -0500

    boolean excercises

---
+++ Boolean Excercises.js
@@ -0,0 +1,54 @@
+//can buy milk
+/*let cash = 50;
+let milkPrice = 40;
+let canBuyMilk = cash > milkPrice;
+console.log(canBuyMilk);*/
+
+//Is Retired
+
+//John is 58 years old, and the retirement age in his country of residence is 65. 
+//Write a program that checks whether John has reached retirement age.
+
+/*let age = 58;
+let retirementAge = 65;
+let isRetired = age === retirementAge;
+console.log(isRetired);*/
+
+// Opposite
+//To pass the exam, a student must score at least 60 out of 100 points
+
+/*let mark = 85;
+let passedTheTest = mark >=60;
+let mustTakeTheTestAgain = !passedTheTest;
+console.log(mustTakeTheTestAgain);*/
+
+//Can Stay Home usage of OR operator
+
+/*const isHoliday = false;
+const isVacation = true;
+const canStayHome = isHoliday || isVacation; 
+console.log(canStayHome);*/
+
+//Make Tea usage of AND operator 
+
+/*const isWaterHot = false;
+const haveTea = true;
+const canMakeTea = isWaterHot && haveTea; 
+console.log(canMakeTea);*/
+
+//Make Cake
+
+/*let hasAllIngredients = true;
+let hasStove = true;
+let canMakeCake = hasAllIngredients && hasStove;
+console.log(canMakeCake);*/
+
+// Go Home ~ operator OR 
+
+let workDone = true;
+let dayFinished = false;
+let canGoHome = workDone || dayFinished;
+console.log(canGoHome);
+
+
+
